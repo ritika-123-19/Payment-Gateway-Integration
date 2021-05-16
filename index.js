3da2e89f2644773d3dc76990eeac6af3bff67104
Ritika Jain <ritika192713@gmail.com>
2021-05-16 16:28:20 +0530

    Update Commit

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = "/success";
+    params['CALLBACK_URL'] = '/success';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
 
